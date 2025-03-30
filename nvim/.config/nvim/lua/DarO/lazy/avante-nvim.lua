791a2cbdea7d0c7b8e37dc46bd44181086b69523
wilo01 <wilk9496@gmail.com>
2025-03-30 20:47:28 +0200

    Adding avante-nvim

---
+++ nvim/.config/nvim/lua/DarO/lazy/avante-nvim.lua
@@ -0,0 +1,48 @@
+return {
+   "yetone/avante.nvim",
+   event = "VeryLazy",
+   version = false,
+   opts = {
+      provider = "claude",
+      claude = {
+         endpoint = "https://api.anthropic.com",
+         model = "claude-3-5-sonnet-20241022",
+         timeout = 30000,
+         temperature = 0,
+         max_tokens = 4096,
+      },
+   },
+   build = "make",
+   dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "echasnovski/mini.pick",
+      "nvim-telescope/telescope.nvim",
+      "hrsh7th/nvim-cmp",
+      "ibhagwan/fzf-lua",
+      "nvim-tree/nvim-web-devicons",
+      "zbirenbaum/copilot.lua",
+      {
+         "HakonHarnes/img-clip.nvim",
+         event = "VeryLazy",
+         opts = {
+            default = {
+               embed_image_as_base64 = false,
+               prompt_for_file_name = false,
+               drag_and_drop = {
+                  insert_mode = true,
+               },
+            },
+         },
+      },
+      {
+         'MeanderingProgrammer/render-markdown.nvim',
+         opts = {
+            file_types = { "markdown", "Avante" },
+         },
+         ft = { "markdown", "Avante" },
+      },
+   },
+}
