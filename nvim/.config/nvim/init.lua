6ca2ddd2a117c8152f55f07b881d2513cd7eb082
wilo01 <wilk9496@gmail.com>
2025-03-31 23:00:26 +0100

    JIRA: #Linux
    Logs:
    - init.lua -> Added version to start-up
    - remap.lua -> Added new remap
    - set.lua -> Added new line regarding new plugin avante

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,7 +4,8 @@ vim.api.nvim_command('autocmd VimEnter * lua print_startup_time()')
 
 function _G.print_startup_time()
    local elapsed_time = (vim.loop.hrtime() - vim.g.startup_time) / 1e6
-   print(string.format("Hello DarO, Neovim startup time: %.2f ms", elapsed_time))
+   local v = vim.version()
+   print(string.format("Hello DarO, Neovim v%d.%d.%d startup time: %.2f ms", v.major, v.minor, v.patch, elapsed_time))
    vim.defer_fn(function()
       vim.cmd('messages')
    end, 50)
