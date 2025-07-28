a6cae4cebf9d81bb5c45a52146364c94340c43b3
wilo01 <wilk9496@gmail.com>
2025-07-28 20:46:44 +0100

    Linux
    Linux
    
    Logs:
    - Neovim update config

--- nvim/.config/nvim/lua/DarO/lazy/lsp.lua
+++ nvim/.config/nvim/lua/DarO/lazy/lsp.lua
@@ -1,36 +1,176 @@
 if vim.fn.has 'nvim-0.11' == 1 then
    return {
-      vim.lsp.enable({
-         "gopls",
-         "lua_ls"
-      }),
-
-      vim.diagnostic.config({
-         virtual_text = true,
-         underline = true,
-         update_in_insert = false,
-         severity_sort = true,
-         float = {
-            focusable = false,
-            style = "minimal",
-            border = "rounded",
-            source = true,
-            header = "",
-            prefix = "",
+      {
+         "williamboman/mason.nvim",
+         config = function()
+            require("mason").setup()
+         end
+      },
+      {
+         "stevearc/conform.nvim",
+         config = function()
+            require("conform").setup({
+               formatters_by_ft = {
+                  go = { "gofmt" },
+                  javascript = { "prettierd" },
+                  typescript = { "prettierd" },
+                  json = { "prettierd" },
+                  yaml = { "prettierd" },
+                  html = { "prettierd" },
+                  markdown = { "prettierd" },
+               },
+               format_on_save = {
+                  timeout_ms = 500,
+                  lsp_fallback = true,
+               },
+            })
+         end
+      },
+      {
+         "hrsh7th/nvim-cmp",
+         dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
          },
-         signs = {
-            text = {
-               [vim.diagnostic.severity.ERROR] = "",
-               [vim.diagnostic.severity.WARN] = "",
-               [vim.diagnostic.severity.INFO] = "",
-               [vim.diagnostic.severity.HINT] = ""
-            },
-            numhl = {
-               [vim.diagnostic.severity.ERROR] = "ErrorMsg",
-               [vim.diagnostic.severity.WARN] = "WarningMsg",
+         config = function()
+            local cmp = require('cmp')
+            local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+            cmp.setup({
+               snippet = {
+                  expand = function(args)
+                     require('luasnip').lsp_expand(args.body)
+                  end,
+               },
+               mapping = cmp.mapping.preset.insert({
+                  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                  ["<C-Space>"] = cmp.mapping.complete(),
+               }),
+               sources = cmp.config.sources({
+                  { name = 'nvim_lsp' },
+                  { name = 'luasnip' },
+               }, {
+                  { name = 'buffer' },
+               })
+            })
+         end
+      },
+      {
+         "folke/lazydev.nvim",
+         ft = "lua",
+         opts = {
+            library = {
+               { path = "${3rd}/luv/library", words = { "vim%.uv" } },
             }
          }
-      })
+      },
+      config = function()
+         local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+         vim.lsp.enable({
+            {
+               name = "gopls",
+               cmd = { "gopls" },
+               root_markers = { "go.mod", ".git" },
+               capabilities = capabilities,
+               settings = {
+                  gopls = {
+                     analyses = {
+                        unusedparams = true,
+                     },
+                     staticcheck = true,
+                  },
+               },
+            },
+            {
+               name = "lua_ls",
+               cmd = { "lua-language-server" },
+               root_markers = { ".luarc.json", ".git" },
+               capabilities = capabilities,
+               settings = {
+                  Lua = {
+                     runtime = { version = "Lua 5.1" },
+                     diagnostics = {
+                        globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+                     }
+                  }
+               }
+            },
+            {
+               name = "eslint",
+               cmd = { "vscode-eslint-language-server", "--stdio" },
+               root_markers = { ".eslintrc.js", ".eslintrc.json", "package.json", ".git" },
+               capabilities = capabilities,
+            },
+            {
+               name = "dockerls",
+               cmd = { "docker-langserver", "--stdio" },
+               root_markers = { "Dockerfile", ".git" },
+               capabilities = capabilities,
+            },
+            {
+               name = "yamlls",
+               cmd = { "yaml-language-server", "--stdio" },
+               root_markers = { ".git" },
+               capabilities = capabilities,
+               settings = {
+                  yaml = {
+                     schemas = {
+                        ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "/docker-compose.yml"
+                     }
+                  }
+               }
+            },
+            {
+               name = "zls",
+               cmd = { "zls" },
+               root_markers = { ".git", "build.zig", "zls.json" },
+               capabilities = capabilities,
+               settings = {
+                  zls = {
+                     enable_inlay_hints = true,
+                     enable_snippets = true,
+                     warn_style = true,
+                  },
+               },
+            }
+         })
+
+         vim.diagnostic.config({
+            virtual_text = true,
+            underline = true,
+            update_in_insert = false,
+            severity_sort = true,
+            float = {
+               focusable = false,
+               style = "minimal",
+               border = "rounded",
+               source = true,
+               header = "",
+               prefix = "",
+            },
+            signs = {
+               text = {
+                  [vim.diagnostic.severity.ERROR] = "",
+                  [vim.diagnostic.severity.WARN] = "",
+                  [vim.diagnostic.severity.INFO] = "",
+                  [vim.diagnostic.severity.HINT] = ""
+               },
+               numhl = {
+                  [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+                  [vim.diagnostic.severity.WARN] = "WarningMsg",
+               }
+            }
+         })
+
+         vim.g.zig_fmt_parse_errors = 0
+         vim.g.zig_fmt_autosave = 0
+      end
    }
 else
    return {
@@ -239,10 +379,10 @@ else
             },
             signs = {
                text = {
-                  [vim.diagnostic.severity.ERROR] = "",
-                  [vim.diagnostic.severity.WARN] = "",
-                  [vim.diagnostic.severity.INFO] = "",
-                  [vim.diagnostic.severity.HINT] = ""
+                  [vim.diagnostic.severity.ERROR] = "",
+                  [vim.diagnostic.severity.WARN] = "",
+                  [vim.diagnostic.severity.INFO] = "",
+                  [vim.diagnostic.severity.HINT] = ""
                },
                numhl = {
                   [vim.diagnostic.severity.ERROR] = "ErrorMsg",
