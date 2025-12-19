41ae6852dcf4f2dd74ee3527ce8b3e03522fb3f2
wilo01 <wilk9496@gmail.com>
2025-12-19 09:12:40 +0000

    Linux
    Linux
    
    Logs:
    - Sway: Set _JAVA_AWT_WM_NONREPARENTING=1 for Java Swing fixes.
    - Sway: Use .jxl background images in idle config.
    - sqldev: Add product.conf, set JavaHome to JDK 17.
    - sqldev: product.conf: Add JDK 17 requirement notes.

--- Sway/.config/sway/idle_config.sh
+++ Sway/.config/sway/idle_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 swayidle -w \
-   timeout 3 "swaylock -i /usr/share/backgrounds/default-dark.png -f" \
+   timeout 3 "swaylock -i /usr/share/backgrounds/default-dark.jxl -f" \
    timeout 6 'swaymsg "output * power off"' \
-   resume 'swaymsg "output * power on"; swaybg -i /usr/share/backgrounds/default-dark.png -m fill' \
-   before-sleep "swaylock -i /usr/share/backgrounds/default-dark.png -f"
+   resume 'swaymsg "output * power on"; swaybg -i /usr/share/backgrounds/default-dark.jxl -m fill' \
+   before-sleep "swaylock -i /usr/share/backgrounds/default-dark.jxl -f"
